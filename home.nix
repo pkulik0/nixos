cca7d89859d3f15ce42f98176caf1835f843ce00
pkulik0 <me@pkulik.com>
2026-01-01 18:02:48 +0100

    Add nvim telescope and blink

--- home.nix
+++ home.nix
@@ -103,6 +103,25 @@ in
           vim.keymap.set('n', '<leader>e', ':Neotree toggle<CR>', { silent = true })
         '';
       }
+      {
+        plugin = blink-cmp;
+        type = "lua";
+        config = ''
+          require('blink-cmp').setup {}
+        '';
+      }
+      {
+        plugin = telescope-nvim;
+        type = "lua";
+        config = ''
+          require('telescope').setup {}
+          local builtin = require('telescope.builtin')
+          vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Find files' })
+          vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Live grep' })
+          vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Find buffers' })
+          vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Help tags' })
+        '';
+      }
     ];
   };
 }
