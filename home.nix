637d1f5f3b28b8b4d4e6edf1859e3a39e69b4f44
pkulik0 <me@pkulik.com>
2026-01-01 20:37:49 +0100

    Add nvim-dap; configure line nums and tabs

--- home.nix
+++ home.nix
@@ -1,4 +1,4 @@
-{ pkgs, ...}:
+{ pkgs, ... }:
 let
   rust-overlay = import (builtins.fetchTarball "https://github.com/oxalica/rust-overlay/archive/6d14586a5917a1ec7f045ac97e6d00c68ea5d9f3.tar.gz");
 in
@@ -38,6 +38,11 @@ in
     black
     nodePackages.prettier
     nixpkgs-fmt
+    ## Debug adapters
+    python3Packages.debugpy
+    delve
+    lldb
+    vscode-js-debug
   ];
 
   programs.zsh = {
@@ -70,7 +75,7 @@ in
     settings = {
       user = {
         name = "pkulik0";
-	      email = "me@pkulik.com";
+        email = "me@pkulik.com";
       };
       init.defaultBranch = "main";
     };
@@ -83,8 +88,20 @@ in
     vimAlias = true;
 
     extraLuaConfig = ''
+      -- Leader	
       vim.g.mapleader = " "
       vim.g.maplocalleader = " "
+
+      -- Indentation settings
+      vim.opt.expandtab = true       -- Use spaces instead of tabs
+      vim.opt.shiftwidth = 2         -- Number of spaces for each indentation level
+      vim.opt.tabstop = 2            -- Number of spaces a tab counts for
+      vim.opt.softtabstop = 2        -- Number of spaces for tab key in insert mode
+      vim.opt.smartindent = true     -- Smart autoindenting on new lines
+
+      -- Line numbers
+      vim.opt.number = true          -- Show absolute line number on current line
+      vim.opt.relativenumber = true  -- Show relative line numbers
     '';
 
     plugins = with pkgs.vimPlugins; [
@@ -217,6 +234,146 @@ in
         type = "lua";
         config = ''
           require('Comment').setup {}
+        '';
+      }
+      {
+        plugin = nvim-dap;
+        type = "lua";
+        config = ''
+          local dap = require('dap')
+
+          -- Python
+          dap.adapters.python = {
+            type = 'executable',
+            command = '${pkgs.python3Packages.debugpy}/bin/python',
+            args = { '-m', 'debugpy.adapter' },
+          }
+          dap.configurations.python = {
+            {
+              type = 'python',
+              request = 'launch',
+              name = 'Launch file',
+              program = "''${file}",
+              pythonPath = '${pkgs.python3}/bin/python',
+            },
+          }
+
+          -- Go
+          dap.adapters.go = {
+            type = 'server',
+            port = "''${port}",
+            executable = {
+              command = '${pkgs.delve}/bin/dlv',
+              args = { 'dap', '-l', '127.0.0.1:''${port}' },
+            },
+          }
+          dap.configurations.go = {
+            {
+              type = 'go',
+              name = 'Debug',
+              request = 'launch',
+              program = "''${file}",
+            },
+            {
+              type = 'go',
+              name = 'Debug Package',
+              request = 'launch',
+              program = "''${fileDirname}",
+            },
+            {
+              type = 'go',
+              name = 'Debug test',
+              request = 'launch',
+              mode = 'test',
+              program = "''${file}",
+            },
+          }
+
+          -- JavaScript/TypeScript
+          dap.adapters['pwa-node'] = {
+            type = 'server',
+            host = 'localhost',
+            port = "''${port}",
+            executable = {
+              command = '${pkgs.vscode-js-debug}/bin/js-debug',
+              args = { "''${port}" },
+            },
+          }
+          dap.configurations.javascript = {
+            {
+              type = 'pwa-node',
+              request = 'launch',
+              name = 'Launch file',
+              program = "''${file}",
+              cwd = "''${workspaceFolder}",
+            },
+          }
+          dap.configurations.typescript = {
+            {
+              type = 'pwa-node',
+              request = 'launch',
+              name = 'Launch file',
+              program = "''${file}",
+              cwd = "''${workspaceFolder}",
+              runtimeExecutable = '${pkgs.nodejs}/bin/node',
+              runtimeArgs = { '-r', 'ts-node/register' },
+            },
+          }
+
+          -- C/C++/Rust
+          dap.adapters.lldb = {
+            type = 'executable',
+            command = '${pkgs.lldb}/bin/lldb-dap',
+            name = 'lldb',
+          }
+          dap.configurations.c = {
+            {
+              name = 'Launch',
+              type = 'lldb',
+              request = 'launch',
+              program = function()
+                return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+              end,
+              cwd = "''${workspaceFolder}",
+              stopOnEntry = false,
+              args = {},
+            },
+          }
+          dap.configurations.cpp = dap.configurations.c
+          dap.configurations.rust = dap.configurations.c
+
+          -- Keybindings
+          vim.keymap.set('n', '<F5>', dap.continue, { desc = 'Debug: Start/Continue' })
+          vim.keymap.set('n', '<F10>', dap.step_over, { desc = 'Debug: Step Over' })
+          vim.keymap.set('n', '<F11>', dap.step_into, { desc = 'Debug: Step Into' })
+          vim.keymap.set('n', '<F12>', dap.step_out, { desc = 'Debug: Step Out' })
+          vim.keymap.set('n', '<leader>b', dap.toggle_breakpoint, { desc = 'Debug: Toggle Breakpoint' })
+          vim.keymap.set('n', '<leader>B', function()
+            dap.set_breakpoint(vim.fn.input('Breakpoint condition: '))
+          end, { desc = 'Debug: Set Conditional Breakpoint' })
+        '';
+      }
+      {
+        plugin = nvim-dap-ui;
+        type = "lua";
+        config = ''
+          local dap, dapui = require('dap'), require('dapui')
+
+          dapui.setup {}
+
+          -- Auto-open UI when debugging starts
+          dap.listeners.after.event_initialized['dapui_config'] = function()
+            dapui.open()
+          end
+          dap.listeners.before.event_terminated['dapui_config'] = function()
+            dapui.close()
+          end
+          dap.listeners.before.event_exited['dapui_config'] = function()
+            dapui.close()
+          end
+
+          -- Toggle UI manually
+          vim.keymap.set('n', '<leader>du', dapui.toggle, { desc = 'Debug: Toggle UI' })
         '';
       }
       {
