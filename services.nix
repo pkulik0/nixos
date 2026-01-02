e03a54469d7cafc8ba698a8b10ebe4697533372a
pkulik0 <me@pkulik.com>
2026-01-02 06:38:00 +0100

    Add more services

--- services.nix
+++ services.nix
@@ -1,22 +1,99 @@
 { pkgs, ... }:
 {
-  services.postgresql = {
-    enable = true;
-    package = pkgs.postgresql_18;
-    ensureDatabases = [ "pk" ];
-    ensureUsers = [
-      {
-        name = "pk";
-        ensureDBOwnership = true;
-      }
-    ];
-  };
+  services = {
+    postgresql = {
+      enable = true;
+      package = pkgs.postgresql_18;
+      ensureDatabases = [ "pk" "postgres" ];
+      ensureUsers = [
+        {
+          name = "pk";
+          ensureDBOwnership = true;
+        }
+        {
+          name = "postgres-exporter";
+        }
+      ];
+    };
 
-  services.dragonflydb = {
-    enable = true;
-  };
+    redis = {
+      servers."" = {
+        enable = true;
+        port = 6379;
+      };
+    };
 
-  services.nats = {
-    enable = true;
+    nats = {
+      enable = true;
+      jetstream = true;
+      settings = {
+        http_port = 8222;
+      };
+    };
+
+    vault = {
+      enable = true;
+      address = "127.0.0.1:8200";
+      extraConfig = ''
+        telemetry {
+          prometheus_retention_time = "30s"
+          disable_hostname = true
+        }
+      '';
+    };
+
+    prometheus = {
+      enable = true;
+      package = pkgs.prometheus;
+      exporters = {
+        postgres = {
+          enable = true;
+          dataSourceName = "user=postgres-exporter database=postgres host=/run/postgresql sslmode=disable";
+        };
+        redis = {
+          enable = true;
+        };
+        nats = {
+          enable = true;
+          extraFlags = [
+            "-varz"
+            "-connz"
+            "-healthz"
+            "-subz"
+            "-routez"
+          ];
+        };
+      };
+      scrapeConfigs = [
+        {
+          job_name = "postgres";
+          static_configs = [{
+            targets = [ "127.0.0.1:9187" ];
+          }];
+        }
+        {
+          job_name = "redis";
+          static_configs = [{
+            targets = [ "127.0.0.1:9121" ];
+          }];
+        }
+        {
+          job_name = "nats";
+          static_configs = [{
+            targets = [ "127.0.0.1:7777" ];
+          }];
+        }
+        {
+          job_name = "vault";
+          metrics_path = "/v1/sys/metrics";
+          params = {
+            format = [ "prometheus" ];
+          };
+          static_configs = [{
+            targets = [ "127.0.0.1:8200" ];
+          }];
+        }
+      ];
+    };
   };
 }
